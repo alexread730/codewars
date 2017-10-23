6c10ac00e53bad318d071a3455f4eadbf0f6cccd
Alex Read <alexread730@gmail.com>
2017-10-23 10:42:26 -0600

    added some to decryption

--- alternating_split_encryption/solution1.js
+++ alternating_split_encryption/solution1.js
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/57814d79a56c88e3e0000786/train/javascript
+
 For building the encrypted string:
 Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
 Do this n times!
